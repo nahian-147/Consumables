8e315b35dedd60637276bd9b54b5cf25dbde1d08
kaziNAHIAN <55456056+nahian-147@users.noreply.github.com>
2021-05-04 21:45:34 +0600

    Add files via upload

--- cons.sql
+++ cons.sql
@@ -5,18 +5,21 @@ use CONSUMABLES;
 create table if not exists consumables  (id int not null auto_increment,
                                          type varchar(10) not null,
                                          name varchar(100) not null,
-                                         consStart date,
+                                         consStart date not null,
                                          consEnd date,
-					 rating float default 0.0,
+					                     rating float default 0.0,
                                          consTime float default 0.0,
-					 total_days int default 0,
-				         primary key(id));
+					                     total_days int default 0,
+				                         primary key(id));
 
-create table if not exists deleteds  (id int not null auto_increment,
+create table if not exists deleteds     (id int not null,
+                                         type varchar(10) not null,
                                          name varchar(100) not null,
-                                         consTime float not null,
-                                         total_days int not null,
-                                         primary key(id));
+                                         consStart date not null,
+                                         consEnd date,
+					                     rating float default 0.0,
+                                         consTime float default 0.0,
+					                     total_days int default 0);
 
 create table if not exists total (total_time float default 0.0,
 			          total_days int default 0);
--- myConsumes.py
+++ myConsumes.py
@@ -1,7 +1,10 @@
 import os
+from datetime import date
 import mysql.connector
 from rich.console import Console
 from rich.table import Column,Table
+
+hs = "bold blue"
 
 db = mysql.connector.connect(
         host='localhost',
@@ -19,79 +22,172 @@ def insrt(t):
     rating = input('rating (out of 10) ? : ')
 
     c = db.cursor(buffered=True)
-    insert = """insert into consumables (type,name) 
-            values ('"""+t+"""','"""+name+"""');"""
-    c.execute(insert)
-    if not start == "":
-        us = "update consumables set consStart = '"+start+"' WHERE name = '"+name+"';"
-        c.execute(us)
-    if not end == "":
-        ue = "update consumables set consEnd = '"+end+"' WHERE name = '"+name+"';"
-        c.execute(ue)
-    if not rating == "":
-        ur = "update consumables set rating = '"+rating+"' WHERE name = '"+name+"';"
-        c.execute(ur)
 
+    if start == "":
+        today = date.today().strftime("%Y-%m-%d")
+        start = str(today)
+    
+    insert = """insert into consumables (type,name,consStart) 
+            values ('"""+t+"""','"""+name+"""','"""+start+"""');"""
+    try:
+        c.execute(insert)
+        if not end == "":
+            ue = "update consumables set consEnd = '"+end+"' WHERE name = '"+name+"';"
+            c.execute(ue)
+        if not rating == "":
+            ur = "update consumables set rating = '"+rating+"' WHERE name = '"+name+"';"
+            c.execute(ur)
+        input("A new Consumable has been added successfully.\nPress any key to return back to the main menu.")
+    except:
+        db.rollback()
+        input("Sorry, Record couldn't be inserted. Please, try again.")
     db.commit()
 
-def addTime(name,hours):
+def addTime(cid,hours):
     c = db.cursor(buffered=True)
-    q1 = "UPDATE consumables SET consTime = consTime + "+hours+" WHERE name  = '"+name+"';"
+    q1 = "UPDATE consumables SET consTime = consTime + "+hours+" WHERE id  = '"+cid+"';"
     q2 = "UPDATE total SET total_time = total_time + "+hours+";"
-    c.execute(q1)
-    c.execute(q2)
-    db.commit()
-
-def aday(name):
-    c = db.cursor(buffered=True)
-    q1 = "UPDATE consumables SET total_days = total_days + 1 WHERE name  = '"+name+"';"
-    q2 = "UPDATE total SET total_days = total_days + 1;"
-    c.execute(q1)
-    c.execute(q2)
-    db.commit()
-
-def chngRating(name,rating):
-    c = db.cursor(buffered=True)
-    q = "UPDATE consumables SET rating = "+rating+" WHERE name  = '"+name+"';"
-    c.execute(q)
-    db.commit()
-
-def chngEdate(name,date):
-    c = db.cursor(buffered=True)
-    q1 = "SELECT consEnd from consumables where name = '"+name+"';"
-    c.execute(q1)
-    if not len(c.fetchall()) :
-        q2 = "UPDATE consumables SET consEnd = '"+date+"' where name = '"+name+"';"
+    try:
+        c.execute(q1)
         c.execute(q2)
-        db.commit()
-    else:
-        print("Can't update now !")
+        input("Time has been added successfully.\nPress any key to return back to the main menu.")
+    except:
+        db.rollback()
+        input("Time could not be updated.Please try again.")
+    db.commit()
 
-def dlt(name):
+def aday(cid):
+    c = db.cursor(buffered=True)
+    q1 = "UPDATE consumables SET total_days = total_days + 1 WHERE id  = '"+str(cid)+"';"
+    q2 = "UPDATE total SET total_days = total_days + 1;"
+    try:
+        c.execute(q1)
+        c.execute(q2)
+        input("A day to the Consumable has been added successfully.\nPress any key to return back to the main menu.")
+    except:
+        db.rollback()
+        input("A day could not be added.Please try again.")
+    db.commit()
+
+def chngRating(cid,rating):
+    c = db.cursor(buffered=True)
+    q = "update consumables set rating = "+rating+" where id  = '"+str(cid)+"';"
+    try:
+        c.execute(q)
+        input("Rating has been updated successfully.\nPress any key to return back to the main menu.")
+    except:
+        db.rollback()
+        input("Rating Could not be updated.Please try again.")
+    db.commit()
+
+def setEdate(cid):
+    date = input("Date ? : ")
     c = db.cursor()
-    fetch = "select id, name, consTime,total_days from consumables where name = '"+name+"';"
+    q1 = "select consEnd from consumables where id = '"+str(cid)+"';"
+    c.execute(q1)
+    p = c.fetchall()
+    print(p,len(p))
+    input()
+    if len(p)-1 == 0:
+        q2 = "update consumables set consEnd = '"+date+"' where id = '"+str(cid)+"';"
+        try:
+            c.execute(q2)
+        except:
+            db.rollback()
+            input("There was an error Setting the Date !\nPress Enter to return back to the main menu.")
+        db.commit()
+        input("Ending Date has been Set successfully.\nPress any key to return back to the main menu.")
+    else:
+        input("Can't Update now !\nPress Enter to return back to the main menu.")
+
+def dlt(cid):
+    
+    c = db.cursor()
+    fetch = "select * from consumables where id = '"+cid+"';"
     c.execute(fetch)
     p = c.fetchone()
-    d = "delete from consumables where id = '"+str(p[0])+"';"
-    insert = """insert into deleteds (name,consTime,total_days) 
-             values ('"""+name+"""','"""+str(p[2])+"""','"""+str(p[3])+"""');"""
-    c.execute(d)
-    c.execute(insert)
-    db.commit()
+    d = "delete from consumables where id = '"+str(cid)+"';"
+    
+    name = p[2]
+    t = p[1]
+    start = str(p[3])
+    end = str(p[4])
+    rating = str(p[5])
+    time = str(p[6])
+    days = str(p[7])
+    
+    insert = """insert into deleteds (id,type,name,consStart,rating,consTime,total_days) 
+            values ('"""+str(cid)+"""','"""+t+"""','"""+name+"""','"""+start+"""','"""+rating+"""','"""+time+"""','"""+days+"""');"""
+    
+    try:
+        c.execute(insert)
+        c.execute(d)
+    except:
+        db.rollback()
+        print("An Error Occurred!!! Please, try again.")
+    else:
+        db.commit()
+        input("Selected Consumable has been Deleted Successfully.\nPress any key to return back to the main menu.")
+    
+
+def rstr(cid):
+    c = db.cursor()
+    fetch = "select * from deleteds where id = '"+str(cid)+"';"
+    c.execute(fetch)
+    p = c.fetchone()
+    d = "delete from deleteds where id = '"+str(cid)+"';"
+    
+    name = p[2]
+    t = p[1]
+    start = str(p[3])
+    end = str(p[4])
+    rating = str(p[5])
+    time = str(p[6])
+    days = str(p[7])
+    
+    insert = """insert into consumables (id,type,name,consStart,rating,consTime,total_days) 
+            values ('"""+str(cid)+"""','"""+t+"""','"""+name+"""','"""+start+"""','"""+rating+"""','"""+time+"""','"""+days+"""');"""
+    try:
+        c.execute(insert)
+        c.execute(d)
+    except:
+        db.rollback()
+        print("An Error Occurred!!! Please, try again.")
+    else:
+        db.commit()
+        input("Selected Consumable has been Restored Successfully.\nPress any key to return back to the main menu.")
 
 def shownames():
     os.system('clear')
     c = db.cursor()
-    q = "select name from consumables;"
+    q = "select id,name from consumables;"
     c.execute(q)
     names = c.fetchall()
 
     console = Console()
-    table = Table(show_header=True, header_style="bold magenta")
+    table = Table(show_header=True, header_style=hs)
+    table.add_column ("ID", justify="left")
     table.add_column ("NAMES", justify="left")
 
     for name in names:
-        table.add_row(str(name[0]))
+        table.add_row(str(name[0]),str(name[1]))
+
+    console.print(table)
+
+def shownames_del():
+    os.system('clear')
+    c = db.cursor()
+    q = "select id,name from deleteds;"
+    c.execute(q)
+    names = c.fetchall()
+
+    console = Console()
+    table = Table(show_header=True, header_style=hs)
+    table.add_column ("ID", justify="left")
+    table.add_column ("NAMES", justify="left")
+
+    for name in names:
+        table.add_row(str(name[0]),str(name[1]))
 
     console.print(table)
 
@@ -118,7 +214,7 @@ def show():
     at = c.fetchone()
 
     console = Console()
-    table = Table(show_header=True, header_style="bold magenta")
+    table = Table(show_header=True, header_style=hs)
     table.add_column ("INFO", justify="left")
     table.add_column("BOOKS", justify="center")
     table.add_column("SERIES", justify="center")
@@ -134,13 +230,14 @@ def show():
 
 
 def showind(typ):
+    os.system('clear')
     c = db.cursor()
     query = "select name,total_days,consTime,rating from consumables where type = '"+typ+"';"
     c.execute(query)
     lst = c.fetchall()
 
     console = Console()
-    table = Table(show_header=True, header_style="bold magenta")
+    table = Table(show_header=True, header_style=hs)
     table.add_column ("NAME", justify="left")
     table.add_column("TOTAL DAYS", justify="center")
     table.add_column("TOTAL TIME (IN HOURS)", justify="center")
@@ -152,14 +249,14 @@ def showind(typ):
     console.print(table)
 
 
-def fulldetail(name):
+def fulldetail(cid):
     c = db.cursor()
-    query = "select * from consumables where name = '"+name+"';"
+    query = "select * from consumables where id = '"+cid+"';"
     c.execute(query)
     lst = c.fetchone()
 
     console = Console()
-    table = Table(show_header=True, header_style="bold magenta")
+    table = Table(show_header=True, header_style=hs)
     table.add_column("ID", style="dim")
     table.add_column("TYPE")
     table.add_column ("NAME", justify="left")
@@ -173,18 +270,67 @@ def fulldetail(name):
 
     console.print(table)
 
+def showoptn():
+    console = Console()
+    table = Table(show_header=True, header_style=hs)
+    table.add_column ("#", justify="left")
+    table.add_column ("CHOICE", justify="left")
+    table.add_row("1","Add a Consumable")
+    table.add_row("2","Edit a Consumable")
+    table.add_row("3","Delete a Consumable")
+    table.add_row("4","Restore a Consumable")
+    table.add_row("5","See the list of consumables and individually")
+    table.add_row("6","See overall info")
+    table.add_row("7","Exit")
+    console.print(table)
 
+def showoptn_1():
+    console = Console()
+    table = Table(show_header=True, header_style=hs)
+    table.add_column ("#", justify="left")
+    table.add_column ("CHOICE", justify="left")
+    table.add_row("1","Books")
+    table.add_row("2","Series")
+    table.add_row("3","Movie")
+    table.add_row("4","None")
+    console.print("Select one Consumable from below : ")
+    console.print(table)
 
-options = "1.Add a Consumable\n2.Edit a Consumable\n3.Delete a Consumable\n4.See the list of consumables and individually\n5.See overall info\n6.Exit\n"
+def showoptn_2():
+    console = Console()
+    table = Table(show_header=True, header_style=hs)
+    table.add_column ("#", justify="left")
+    table.add_column ("Actions", justify="left")
+    table.add_row("1","Add time in Hours")
+    table.add_row("2","Add a day")
+    table.add_row("3","Change the Rating of a Specific Consumable")
+    table.add_row("4","Change Consumption Ening date")
+    table.add_row("5","None")
+    console.print("Select one Action from below : ")
+    console.print(table)
+
+def showoptn_5():
+    console = Console()
+    table = Table(show_header=True, header_style=hs)
+    table.add_column ("#", justify="left")
+    table.add_column ("Actions", justify="left")
+    table.add_row("1","Book")
+    table.add_row("2","Series")
+    table.add_row("3","Movie")
+    table.add_row("4","Individual")
+    table.add_row("5","None")
+    console.print("Select one Option from below : ")
+    console.print(table)
 
 while True:
     os.system('clear')
-    choice = input(options)
-    if(not choice.lower() in ['6','exit']):
+    showoptn()
+    choice = input("Please type in your choice number : ")
+    if(not choice.lower() in ['7','exit']):
         if(choice=='1'):
-            typeOptions = 'Select one Consumable from below: \n1.Books\n2.Series\n3.Movie\n4.None\n'
             os.system('clear')
-            typ = input(typeOptions)
+            showoptn_1()
+            typ = input()
             if not typ.lower() in ['4','none']:
                 if(typ=='1'):
                     insrt('Book')
@@ -195,39 +341,44 @@ while True:
             else:
                 continue
         elif(choice=='2'):
-            
-            actions = 'Select one of the actions below: \n1.Add time in Hours\n2.Add a day\n3.Change the Rating\n4.Change Conumption Ening date\n5.None\n'
             os.system('clear')
             shownames()
-            name = input("Enter the name of the Consumable(Book/Series/Movie) : ")
-            action = input(actions)
+            cid = input("Enter the id of the Consumable : ")
+            showoptn_2()
+            action = input()
             if not action.lower() in ['5','none']:
                 if action == '1':
                     hours = input("How many Hours ? : ")
-                    addTime(name,hours)
+                    addTime(cid,hours)
                 elif action == '2':
-                    aday(name)
+                    aday(cid)
                 elif action =='3':
                     rating = input("New Rating ? : ")
-                    chngRating(name,rating)
+                    chngRating(cid,rating)
                 elif action == '4':
-                    date = input("Date ? : ")
-                    chngEdate(name,date)
+                    setEdate(cid)
             else:
                 continue
         elif(choice=='3'):
-            typeOptions = 'Type the name of the Consumable\nType none or -1 to cancel and return to the main menu\n'
-            name = input(typeOptions)
-            if not name.lower() in ['none','-1']:
-                dlt(name)
-                print('Record Has been Dleted !!!')
-                input("Press any key to return to the Main Menu")
+            shownames()
+            typeOptions = 'Type the ID of the Consumable You want to Delete !!!\nType none or -1 to cancel and return to the main menu\n'
+            cid = input(typeOptions)
+            if not cid.lower() in ['none','-1']:
+                dlt(cid)
             else:
                 continue
         elif(choice=='4'):
-            typeOptions = 'Select one Consumable from below: \n1.Book\n2.Series\n3.Movie\n4.Pick One\n5.None\n'
+            shownames_del()
+            typeOptions = 'Type the ID of the Consumable You want to Restore !!!\nType none or -1 to cancel and return to the main menu\n'
+            cid = input(typeOptions)
+            if not cid.lower() in ['none','-1']:
+                rstr(cid)
+            else:
+                continue
+        elif(choice=='5'):
             os.system('clear')
-            typ = input(typeOptions)
+            showoptn_5()
+            typ = input()
             if not typ.lower() in ['5','none']:
                 if(typ=='1'):
                     showind('Book')
@@ -237,16 +388,15 @@ while True:
                     showind('Movie')
                 elif typ == '4':
                     shownames()
-                    name = input("Enter the name of the consumable : ")
-                    fulldetail(name)
-                input('\nPress any key to return to the Main Menu')
+                    cid = input("Enter the id of the consumable : ")
+                    fulldetail(cid)
+                input('\nPress any key to return back to the main menu.')
             else:
                 continue
-        elif(choice=='5'):
+        elif(choice=='6'):
             show()
-            response = input("\nPress Any key to go to the Main menu")
-            if response.lower() in ['1','cancel']:
-                continue
+            response = input("\nPress any key to return back to the main menu.")
+            continue
     else:
         break
 
